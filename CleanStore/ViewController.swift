dc0b7d06f73da52d249be14b6df07bf1e9de21c3
Jordan Anderson <jordananderson@Jordans-MacBook-Pro.local>
2019-03-24 22:44:55 -0400

    initial commit

---
+++ CleanStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleanStore
+//
+//  Created by Jordan Anderson on 3/17/19.
+//  Copyright © 2019 Dub Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
